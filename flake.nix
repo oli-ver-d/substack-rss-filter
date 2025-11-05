982598912ce155bead264634d1ec5ede3c6dadaa
Oliver Dennis <oliver.d.333@proton.me>
2025-11-05 13:35:11 +0000

    Added support for building and running via nix

--- flake.nix
+++ flake.nix
@@ -10,6 +10,25 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "substack-rss-filter";
+      version = "0.1.0";
+
+      src = ./.;
+
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+      };
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+
+      buildInputs = with pkgs; [
+        openssl
+      ];
+    };
+
     devShells.${system}.default = pkgs.mkShell {
       packages = with pkgs; [
         pkg-config
